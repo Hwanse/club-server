375e64ebae95bfb848c278beeb564dd854b1433e
hwanse <khsb2012@gmail.com>
2021-06-23 20:33:04 +0900

    feat: 지역 데이터 초기화 스크립트 추가

--- src/main/java/com/study/clubserver/domain/zone/Zone.java
+++ src/main/java/com/study/clubserver/domain/zone/Zone.java
@@ -3,6 +3,8 @@ package com.study.clubserver.domain.zone;
 import com.study.clubserver.domain.CommonEntity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import lombok.Getter;
 
 @Entity
@@ -12,7 +14,11 @@ public class Zone extends CommonEntity {
   @Column(nullable = false)
   private String name;
 
-  private String parentZoneName;
+  @ManyToOne
+  @JoinColumn(name = "parent_zone_id")
+  private Zone parentZone;
+
+  private String fullName;
 
   private int level;
 
