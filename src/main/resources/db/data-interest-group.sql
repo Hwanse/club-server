f4137b75c229545431d35089f71e0216f0b0eee5
Hwanse <50852963+Hwanse@users.noreply.github.com>
2021-06-23 20:25:20 +0900

    Delete src/main/resources/db directory

--- src/main/resources/db/data-interest-group.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO INTEREST_COLLECTION
-    ( ID, created_at, updated_at, name)
-values
-    (1, now(), now(), '운동'),
-    (2, now(), now(), '게임'),
-    (3, now(), now(), '문화'),
-    (4, now(), now(), '국내여행'),
-    (5, now(), now(), '해외여행');
